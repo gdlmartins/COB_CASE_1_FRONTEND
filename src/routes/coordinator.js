9713adb9ddc4d70e0e3e5cc276418d978bcb8d5b
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-22 22:00:39 +0800

    coodinator routers

---
+++ src/routes/coordinator.js
@@ -0,0 +1,12 @@
+export const gotoCompetitor =(navigate) => { 
+    navigate("/competitor/")
+}
+export const gotoRanking =(navigate) => { 
+    navigate("/competitor/ranking")
+}
+export const gotoCompetition =(navigate) => { 
+    navigate("/competition ")
+}
+export const gotoCompetitionStatus =(navigate) => { 
+    navigate("/competition/status")
+}
