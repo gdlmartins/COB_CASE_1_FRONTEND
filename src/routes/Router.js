96c4763be711b962c5e2f0fcd14c9a104a69819e
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-22 11:09:49 +0800

    starting

---
+++ src/routes/Router.js
@@ -0,0 +1,14 @@
+import { Route, Routes  } from "react-router";
+
+const Router =()=> { 
+
+    return(
+        <Routes>
+            <Route path="/competitor" element ={} />
+            <Route path="/competitor" element ={} />
+            <Route path="/*" element ={<Error /> }/>
+        </Routes>
+    );
+};
+export default Router; 
+
